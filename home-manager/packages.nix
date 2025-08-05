39df59a81f81c89d24328d6f812648fc51881e3d
faizhasim <faizhasim@gmail.com>
2025-08-05 09:35:38 +0800

    more packages

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -6,13 +6,17 @@ with pkgs;
   aerospace
   aspell # Spell checker
   aspellDicts.en # English dictionary for aspell
+  awscli2
 
   # B
   bat # Cat clone with syntax highlighting
+  bind # nettools
   btop # System monitor and process viewer
 
   # C
-  coreutils # Basic file/text/shell utilities
+  corepack # node wrappers
+  coreutils-full # multiple tools
+  comma # run nix binaries on demand
   curl
 
   # D
@@ -20,30 +24,48 @@ with pkgs;
   direnv # Environment variable management per directory
   difftastic # Structural diff tool
   # discord
+  dive # analyse docker images
   dockutil # Manage icons in the dock
   du-dust # Disk usage analyzer
+  duf # disk usage
 
   # F
   fd # Fast find alternative
-  ffmpeg # Multimedia framework
+  fblog # json log viewer
+  ffmpeg # video editing and cutting
+  findutils
   fswatch # File change monitor
   fzf # Fuzzy finder
 
   # G
-  gcc # GNU Compiler Collection
-  git
+  gnugrep
+  gnumake
+  gnused
+  gnutar
+  golangci-lint
+  gping # ping with a graph
+  gzip
+  gawk
   gh # GitHub CLI
+  git
+  git-lfs
+  gitAndTools.delta # pretty diff tool
   glow # Markdown renderer for terminal
   gnupg # GNU Privacy Guard
   gopls # Go language server
+  graph-easy # draw graphs in the terminal
+  grc # colored log output
+  gcc # GNU Compiler Collection
 
   # H
   htop # Interactive process viewer
+  httpie # awesome alternative to curl
   # hunspell # Spell checker
 
   # I
   iftop # Network bandwidth monitor
   iina
+  inetutils # net tools like ping, traceroute, etc.
   # imagemagick # Image manipulation toolkit
 
   # J
@@ -53,8 +75,14 @@ with pkgs;
   jq # JSON processor
 
   # K
+  k6 # load testing tool
   keycastr
+  kind # k8s in docker
   killall # Kill processes by name
+  krew # kubectl plugins
+  kubectl
+  kubernetes-helm # deploy applications
+  kubie # fzf kubeconfig browser
 
   # L
   lnav # Log file navigator
@@ -69,6 +97,7 @@ with pkgs;
   neovim
   neofetch # System information tool
   neohtop # nice htop GUI alternative
+  nmap # net tools
   ngrok # Secure tunneling service, unfree
 
   # O
@@ -78,12 +107,24 @@ with pkgs;
 
   # P
   pandoc # Document converter
+  presenterm # presentation tool
+  python3
+
+  # R
+  rclone # sync files
+  ripgrep # fast search
+  rustup # rust
 
   # S
   slack # unfree, use brew cask instead
+  slides
   sniffnet # monitor network traffic
+  sshfs # mount folders via ssh
+  stow
 
   # T
+  tealdeer # community driven man pages
+  termdown # terminal countdown
   terraform # Infrastructure as code tool
   terraform-docs
   # terraform-ls # Terraform language server
@@ -104,6 +145,10 @@ with pkgs;
   # W
   wezterm # Terminal emulator
   wget
+
+  # Y
+  yq-go
+  yt-dlp
 
   # Z
   zip # ZIP archive creator
