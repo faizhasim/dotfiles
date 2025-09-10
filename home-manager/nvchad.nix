e05d35dd11cdad607d7be71bf23044b33c8e971e
faizhasim <faizhasim@gmail.com>
2025-09-10 17:58:19 +0800

    transparent nvchad and aerospace app in popup

--- home-manager/nvchad.nix
+++ home-manager/nvchad.nix
@@ -2,5 +2,95 @@
   imports = [
     inputs.nix4nvchad.homeManagerModule
   ];
-  programs.nvchad.enable = true;
+  programs.nvchad = {
+    enable = true;
+    chadrcConfig = ''
+    -- This file needs to have same structure as nvconfig.lua
+    -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+    -- Please read that file to know all available options :(
+
+    ---@type ChadrcConfig
+    local M = {}
+
+    M.base46 = {
+    	theme = "nord",
+
+    	-- hl_override = {
+    	-- 	Comment = { italic = true },
+    	-- 	["@comment"] = { italic = true },
+    	-- },
+    }
+
+    -- M.nvdash = { load_on_startup = true }
+    -- M.ui = {
+    --       tabufline = {
+    --          lazyload = false
+    --      }
+    -- }
+
+    return M
+    '';
+    extraPlugins = ''
+    return {
+       {
+          "xiyaowong/transparent.nvim",
+          event = "VeryLazy",
+          enabled = true,
+          config = function()
+            require("transparent").clear_prefix("WinBar")
+            require("transparent").clear_prefix("Navic")
+            require("transparent").clear_prefix("Float")
+            require("transparent").clear_prefix("Normal")
+            require("transparent").clear_prefix("NeoTree")
+            require("transparent").clear_prefix("Noice")
+            require("transparent").clear_prefix("Notify")
+            require("transparent").clear_prefix("GitSigns")
+            require("transparent").clear_prefix("Mini")
+            require("transparent").clear_prefix("WhichKey")
+            require("transparent").clear_prefix("BufferLine")
+            require("transparent").clear_prefix("Tab")
+            require("transparent").clear_prefix("Telescope")
+            require("transparent").setup({ -- Optional, you don't have to run setup.
+              groups = { -- table: default groups
+                "Comment",
+                "Constant",
+                "TabLineFill",
+                "Special",
+                "WinBar",
+                "Identifier",
+                "Statement",
+                "PreProc",
+                "Type",
+                "Underlined",
+                "Todo",
+                "String",
+                "Function",
+                "Conditional",
+                "Repeat",
+                "Operator",
+                "Structure",
+                "LineNr",
+                "NonText",
+                "SignColumn",
+                "StatusLine",
+                "StatusLineNC",
+                "EndOfBuffer",
+                "SignColumn",
+                "HoverNormal",
+              },
+              extra_groups = {
+                "OctoEditable",
+                "EndOfBuffer",
+                "Search",
+                "Cursor",
+                "LazyNormal",
+              }, -- table: additional groups that should be cleared
+              exclude_groups = {}, -- table: groups you don't want to clear
+              on_clear = function() end,
+            })
+          end,
+      },
+    }
+    '';
+  };
 }
--- home-manager/sketchybar/plugins/aerospace.sh
+++ home-manager/sketchybar/plugins/aerospace.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env zsh
 
+set_workspace_apps_label() {
+  local WORKSPACE_NUM
+  WORKSPACE_NUM=$(aerospace list-workspaces --focused)
+
+  local APPS
+  APPS=$(aerospace list-windows --workspace "$WORKSPACE_NUM" --format "%{app-name}")
+
+  local APPS_ONE_LINE
+  APPS_ONE_LINE=$(printf "%s" "$APPS" | paste -sd "," - | sed 's/,/, /g')
+
+  sketchybar --set "$NAME.apps" label="$APPS_ONE_LINE "
+}
+
 sketchybar --set "$NAME" label="$(aerospace list-workspaces --focused)"
+if [ "$SENDER" = "mouse.entered" ]; then
+  set_workspace_apps_label
+  sketchybar --set "$NAME" popup.drawing=on
+elif [ "$SENDER" = "mouse.exited" ]; then
+  sketchybar --set "$NAME" popup.drawing=off
+fi
+
