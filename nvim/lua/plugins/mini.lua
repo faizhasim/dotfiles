f2ea2c48eb53ad13672d9c6e0d031ee667692ef1
Mohd Faiz Hasim <faizhasim@gmail.com>
2025-11-01 20:18:59 +0800

    Switch back to snacks explorer as default
    - I just need to visualise the directory structure to code.
    - Leaving mini.files for now to ease fs changes

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,22 +1,10 @@
 return {
   {
-    "folke/snacks.nvim",
-    opts = function(_, opts)
-      opts.explorer.enabled = false
-    end,
-    keys = {
-      { "<leader>fe", false },
-      { "<leader>fE", false },
-      { "<leader>E", false },
-      { "<leader>e", false },
-    },
-  },
-  {
     "nvim-mini/mini.files",
     lazy = false,
     opts = {
       options = {
-        use_as_default_explorer = true,
+        use_as_default_explorer = false,
       },
     },
   },
