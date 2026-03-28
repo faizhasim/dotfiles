ea6180811e1525ab1f69ae81d10a5d1275bd9b41
Mohd Faiz Hasim <faizhasim@gmail.com>
2026-03-28 16:12:55 +0800

    chore: Add AcceptEnv for wezterm ssh remote session

--- darwin/default.nix
+++ darwin/default.nix
@@ -12,6 +12,11 @@
       EDITOR = "nvim";
       VISUAL = "nvim";
     };
+
+    # Allow WezTerm to set environment variables for remote pane integration
+    etc."ssh/sshd_config.d/99-wezterm.conf".text = ''
+      AcceptEnv WEZTERM_REMOTE_PANE WEZTERM_PANE WEZTERM_UNIX_SOCKET WEZTERM_SSH
+    '';
   };
 
   # Enable zsh as the default shell, but let home-manager manage it in ./home-manager/zsh.nix
